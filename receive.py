669dde7d656568815ca71fd268533d2791b6e4da
phoenixthrush <mr.cetto@phoenixthrush.com>
2024-12-05 18:21:22 +0100

    restructure functions

--- receive.py
+++ receive.py
@@ -1,28 +1,39 @@
 import cv2
 import time
 
+BIT_LENGHT = 24  # TODO - fetch this on calibrate
+
 camera = cv2.VideoCapture(0)
 
 try:
-    while True:
-        time.sleep(1)
-        ret, frame = camera.read()
+    # contains "lol"
+    # binary_array = [0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0]
 
+    binary_array = []
+
+    for i in range(BIT_LENGHT):
+        time.sleep(0.5)
+        ret, frame = camera.read()
         if ret:
             height, width, _ = frame.shape
             mid_pixel = frame[height // 2, width // 2]
 
-            print()
-            print(mid_pixel[0])  # B
-            print(mid_pixel[1])  # G
-            print(mid_pixel[2])  # R
-
-            if mid_pixel[0] < 128 and mid_pixel[1] < 128 and mid_pixel[2] < 128:
-                print("Black")
+            if mid_pixel[0] >= 128 or mid_pixel[1] >= 128 or mid_pixel[2] >= 128:
+                binary_value = 1
             else:
-                print("White")
+                binary_value = 0
+
+            print(f"{i:02}: {binary_value}")
+            binary_array.append(binary_value)
+
+    binary_string = ''.join(map(str, binary_array))
+    decimal_value = int(binary_string, 2)
+    byte_value = decimal_value.to_bytes(len(binary_string) // 8, 'big')
+    utf8_text = byte_value.decode('utf-8', errors='ignore')
+
+    print(utf8_text)
 except KeyboardInterrupt:
-    print("Exiting...")
+    pass
 finally:
     camera.release()
     cv2.destroyAllWindows()
--- send.py
+++ send.py
@@ -3,11 +3,17 @@ import tkinter as tk
 
 def display_binary_string(binary_string):
     def update_color():
-        nonlocal index
-        if index < len(binary_string):
-            bit = binary_string[index]
-            root.configure(bg="black" if bit == "0" else "white")
-            index += 1
+        if index[0] < len(binary_string):
+            bit = binary_string[index[0]]
+
+            if bit == "0":
+                print(f"{index[0]:02}: {bit}")
+                root.configure(bg="black")
+            else:
+                print(f"{index[0]:02}: {bit}")
+                root.configure(bg="white")
+
+            index[0] += 1
             root.after(500, update_color)
         else:
             root.destroy()
@@ -17,14 +23,13 @@ def display_binary_string(binary_string):
     root.configure(bg="white")
     root.bind('<Escape>', lambda e: root.destroy())
 
-    index = 0
+    index = [0]
     update_color()
 
     root.mainloop()
 
 
-x = "hello"
+x = "lol"
 binary_string = ''.join(f"0{format(ord(char), 'b')}" for char in x)
-
 
 display_binary_string(binary_string)
