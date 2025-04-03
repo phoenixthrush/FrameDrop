12209a799b7ec1500c3819c60271b07737d9f788
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-04-03 12:47:14 +0200

    send char by char

--- src/send.py
+++ src/send.py
@@ -1,17 +1,23 @@
+import time
 import qrcode
 
 with open('data/data.txt', 'r') as file:
     data = file.read().strip()
 
-qr = qrcode.QRCode(
-    version=40,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=1,
-)
+print(data)
 
-qr.add_data(data)
-qr.make(fit=True)
+for char in data:
+    qr = qrcode.QRCode(
+        version=40,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=1,
+    )
 
-img = qr.make_image(fill_color="black", back_color="white")
-img.show()
+    qr.add_data(char)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.show()
+
+    time.sleep(1)
