377c9096ac4a0404721543739fbbe7fb309279ad
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-04-03 12:32:38 +0200

    fix receive

--- src/receive.py
+++ src/receive.py
@@ -1,11 +1,13 @@
 from pyzbar.pyzbar import decode
-from PIL import Image
 from cv2 import VideoCapture
-
 
 cam = VideoCapture(0)
 _, image = cam.read() 
 
 decoded_objects = decode(image)
-for obj in decoded_objects:
-    print(f"Data: {obj.data.decode('utf-8')}")
+
+if decoded_objects:
+    for obj in decoded_objects:
+        print(f"Data: {obj.data.decode('utf-8')}")
+else:
+    print("No QR Code found")
