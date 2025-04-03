3eb9ff91c2b5ebf0e1c00ecf3b5f1a7a5a0dfe5e
phoenixthrush <mr.cetto@phoenixthrush.com>
2025-04-03 12:43:16 +0200

    read data from file instead

--- src/send.py
+++ src/send.py
@@ -1,5 +1,8 @@
 import qrcode
 
+with open('data/data.txt', 'r') as file:
+    data = file.read().strip()
+
 qr = qrcode.QRCode(
     version=40,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -7,7 +10,7 @@ qr = qrcode.QRCode(
     border=1,
 )
 
-qr.add_data('Aylin Bayram')
+qr.add_data(data)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
